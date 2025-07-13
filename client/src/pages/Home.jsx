3e7d18bcb581c12f73a77df7817e2d36e5b0541f
Saurav Anil Sultane <127301681+sauravsultane@users.noreply.github.com>
2025-07-13 23:16:16 +0530

    Home page is created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 const Home = () => {
   return (
     <div>
     <NavBar/>
-    
+    <Hero/>
+    <AiTools/>
     </div>
   )
 }
